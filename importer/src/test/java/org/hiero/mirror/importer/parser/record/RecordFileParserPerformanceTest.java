9f9d77ae1a6e9eee22a2e86e3642cb08577db7d4
Jesse Nelson <jesse@hashgraph.com>
2025-07-30 11:21:01 -0500

    update for restart
    Signed-off-by: Jesse Nelson <jesse@hashgraph.com>

--- importer/src/test/java/org/hiero/mirror/importer/parser/record/RecordFileParserPerformanceTest.java
+++ importer/src/test/java/org/hiero/mirror/importer/parser/record/RecordFileParserPerformanceTest.java
@@ -115,7 +115,7 @@ class RecordFileParserPerformanceTest {
                     .record(r -> r.mergeTransferList(transferList));
         };
 
-        long numCreatedAccounts = 749790002;
+        long numCreatedAccounts = 897000002;
         while (numCreatedAccounts < numAccounts) {
             long count = Math.min(numAccounts - numCreatedAccounts, 30_000L);
             var recordFile = recordFileBuilder
